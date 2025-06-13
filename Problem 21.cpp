13b42d5641c106995a33f52deba4ceecd78e610f
412410043 <167674177+412410043@users.noreply.github.com>
2025-06-13 18:24:25 +0800

    Update and rename Problem 21.c to Problem 21.cpp

---
+++ Problem 21.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a,b,c,x,y;
+    cin>>a>>b>>c>>x>>y;
+
+    if(a*x+b*y+c==0)
+    cout<<("YES\n");
+    else
+    cout<<("NO\n");
+
+	return 0;
+}
