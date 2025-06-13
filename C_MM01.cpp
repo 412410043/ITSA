9a6bdae964d381f7a2d207ca4a8dcd6cb06713ff
412410043 <167674177+412410043@users.noreply.github.com>
2025-06-13 18:08:21 +0800

    Update and rename C_MM01.c to C_MM01.cpp

---
+++ C_MM01.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    double a,b;
+    cin>>a>>b;
+
+    double area;
+    area=a*b/2;
+
+    cout<<fixed<<setprecision(1)<<area<<"\n";
+	return 0;
+}
