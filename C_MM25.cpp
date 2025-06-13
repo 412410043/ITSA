c3fef14b20285e1a32ab594889bcd52fdce47c0c
412410043 <167674177+412410043@users.noreply.github.com>
2025-06-13 18:13:21 +0800

    Update and rename C_MM25.c to C_MM25.cpp

--- C_MM25.cpp
+++ C_MM25.cpp
@@ -1,16 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main(int argc, char *argv[]) {
     int a,sum=0;
-    scanf("%d",&a);
+    cin>>a;
 
     for(int i=1;i<=a;++i){
         if(i%3==0)
         sum+=i;
     }
 
-    printf("%d\n",sum);
+    cout<<sum<<"\n";
 
 	return 0;
 }
