86a7bac4891aa764b0b0e74dc7e5d62d462a64e2
412410043 <167674177+412410043@users.noreply.github.com>
2025-06-13 18:21:54 +0800

    Update and rename Problem 1.c to Problem 1.cpp

---
+++ Problem 1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a;
+    cin>>a;
+    if(a>0)
+    cout<<("正數\n");
+    else if(a<0)
+    cout<<("負數\n");
+    else
+    cout<<("0\n");
+    
+	return 0;
+}
