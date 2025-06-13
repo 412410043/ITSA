038a9955b2ac688deedbe6253976035048c1a294
412410043 <167674177+412410043@users.noreply.github.com>
2025-06-13 18:11:30 +0800

    Update and rename C_MM11.c to C_MM11.cpp

--- C_MM11.cpp
+++ C_MM11.cpp
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main(int argc, char *argv[]) {
     int a,d[3]={0};
-    scanf("%d",&a);
+    cin>>a;
 
     while(a>=10){
         a-=10;
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
         ++d[2];
     }
 
-    printf("NT10=%d\nNT5=%d\nNT1=%d\n",d[0],d[1],d[2]);
+    cout<<"NT10="<<d[0]<<"\nNT5="<<d[1]<<"\nNT1="<<d[2]<<"\n";
 
 	return 0;
 }
