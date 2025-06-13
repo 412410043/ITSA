9945b8d86504898cd5bd43ebd6765c077f017787
412410043 <167674177+412410043@users.noreply.github.com>
2025-06-13 18:13:53 +0800

    Update and rename C_MM26.c to C_MM26.cpp

---
+++ C_MM26.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a,sum=0;
+    cin>>a;
+
+    for(int i=1;i<=a;++i)
+    cout<<i<<"*"<<i<<"="<<i*i<<"\n";
+
+	return 0;
+}
