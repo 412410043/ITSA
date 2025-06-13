3180eda9d47efa905fe6c13c6df44b8d02772df6
412410043 <167674177+412410043@users.noreply.github.com>
2025-06-13 18:11:08 +0800

    Update and rename C_MM09.c to C_MM09.cpp

---
+++ C_MM09.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a;
+    cin>>a;
+    if(a>31)
+    cout<<"Value of more than 31\n";
+
+    else
+    cout<<(1<<a)<<"\n";
+
+
+
+
+	return 0;
+}
