da256fb65fd62f2723ef7f781e3206a8a315898f
412410043 <167674177+412410043@users.noreply.github.com>
2025-06-13 18:12:19 +0800

    Update and rename C_MM15.c to C_MM15.cpp

---
+++ C_MM15.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int x,y;
+    cin>>x>>y;
+
+    if(x<=100 && x>=0 && y>=0 && y<=100)
+    cout<<"inside\n";
+    else
+cout<<"outside\n";
+	return 0;
+}
