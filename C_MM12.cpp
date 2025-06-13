359228c66e762f48faba896f315e658f68ab9871
412410043 <167674177+412410043@users.noreply.github.com>
2025-06-13 18:25:13 +0800

    Update and rename C_MM12.c to C_MM12.cpp

---
+++ C_MM12.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a;
+    cin>>a;
+
+    if(a*1000%238!=0)
+    cout<<a*1000/238+1<<"\n";
+    else
+    cout<<a*1000/238<<"\n";
+
+	return 0;
+}
